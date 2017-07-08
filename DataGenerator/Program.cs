c5a4fc9419890e1af5b235e19220d8863e6aa7db
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-07-08 17:42:50 +0300

    DataGenerator now copies generated files to DataForTest directory.

--- DataGenerator/Program.cs
+++ DataGenerator/Program.cs
@@ -13,11 +13,14 @@ namespace DataGeneratorConsole
 {
     class Program
     {
+        public static Dictionary<string, string> FilePaths = new Dictionary<string, string>();
+        private const string ConnectionString = "server=localhost;port=3306;uid=testuser;password=testpassword; initial catalog=autoservicedb;";
+
         static void Main(string[] args)
         {
+            Initialize();
             DataGenerator generator = new DataGenerator(true);
             Random r = new Random();
-
 
             Console.WriteLine("Number Customers");
             int countCustomers = int.Parse(Console.ReadLine());
@@ -27,23 +30,45 @@ namespace DataGeneratorConsole
             generator.GenerateCustomer(countCustomers, r);
             generator.GenerateOrder(countOrders, r);
 
-            const string connectionString = "server=localhost;port=3306;uid=testuser;password=testpassword; initial catalog=autoservicedb;";
-            IRepository repo;
-            //repo = new XmlRepository(new XmlRepositorySettings("AutoService3.xml", FileMode.Create));
-            //repo = new BinaryRepository(new DataAccess.RepositoryFile.FileRepositorySettings("AutoService.dat", FileMode.Create));
-            repo = new DatabaseRepository(new DatabaseRepositorySettings(connectionString, DatabaseConnectionAction.Create));
-
-            foreach (var item in generator.Customers)
+            List<IRepository> repositories = new List<IRepository>
             {
-                repo.AddCustomer(item);
-            }
-            foreach (var item in generator.Orders)
-            {
-                repo.AddOrder(item);
-            }
-            repo.SaveChanges();
+                new XmlRepository(new XmlRepositorySettings(FilePaths["xml"], FileMode.Create)),
+                new BinaryRepository(new BinaryRepositorySettings(FilePaths["binary"], FileMode.Create)),
+                new DatabaseRepository(
+                    new DatabaseRepositorySettings(ConnectionString, DatabaseConnectionAction.Create))
+            };
 
+            foreach (IRepository repo in repositories)
+            {
+                foreach (var item in generator.Customers)
+                {
+                    repo.AddCustomer(item);
+                }
+                foreach (var item in generator.Orders)
+                {
+                    repo.AddOrder(item);
+                }
+                repo.SaveChanges();
+            }
+
+            CopyFiles();
         }
 
+        private static void CopyFiles()
+        {
+            const string prefixNewPath = @"../../../DataForTest/";
+            foreach (var sourcePath in FilePaths)
+            {
+                string oldPath = sourcePath.Value;
+                string newPath = prefixNewPath + sourcePath.Value;
+                File.Copy(oldPath, newPath);
+            }
+        }
+
+        private static void Initialize()
+        {
+            FilePaths.Add("xml", "AutoService.xml");
+            FilePaths.Add("binary", "AutoService.dat");
+        }
     }
 }
