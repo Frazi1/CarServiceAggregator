2752548654459dd5a33573fbd7b8f6c16f6afdb8
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-07-08 12:54:23 +0300

    Introduced Lazy Repository

---
+++ DataAccess/Repository/LazyRepository/LazyRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccess.Model;
+
+namespace DataAccess.Repository.LazyRepository
+{
+    public class LazyRepository<T> : IRepository
+        where T : IRepository
+    {
+        private readonly Lazy<T> _repositoryLazy;
+
+        public IEnumerable<Customer> Customers => Repository.Customers;
+        public IEnumerable<Order> Orders => Repository.Orders;
+        public T Repository => _repositoryLazy.Value;
+
+        public LazyRepository(RepositorySettings settings)
+        {
+            _repositoryLazy = new Lazy<T>(() => (T)Activator.CreateInstance(typeof(T), settings));
+        }
+
+        public void AddOrder(Order order)
+        {
+            Repository.AddOrder(order);
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            Repository.AddCustomer(customer);
+        }
+
+        public void SaveChanges()
+        {
+            Repository.SaveChanges();
+        }
+    }
+}
