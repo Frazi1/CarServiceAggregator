a4242c2c35be7326698dec10928b45cbab48c5c8
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-07-12 09:15:36 +0300

    Mutable tuple of 3 items.

---
+++ DataAccess/MutableTuple/MutableTuple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataAccess.MutableTuple
+{
+    public class MutableTuple<T1, T2, T3>
+    {
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+        public T3 Item3 { get; set; }
+
+        public static implicit operator MutableTuple<T1, T2, T3>(Tuple<T1, T2, T3> tuple)
+        {
+            return new MutableTuple<T1, T2, T3>
+            {
+                Item1 = tuple.Item1,
+                Item2 = tuple.Item2,
+                Item3 = tuple.Item3
+            };
+        }
+
+        public static implicit operator Tuple<T1, T2, T3>(MutableTuple<T1, T2, T3> mutableTuple)
+        {
+            return new Tuple<T1, T2, T3>(mutableTuple.Item1,
+               mutableTuple.Item2,
+               mutableTuple.Item3);
+        }
+    }
+}
