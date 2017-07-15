03a9c83e5847efc11eb40c189694f4a05bd1813b
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-07-15 20:33:52 +0300

    Removed redundant constructor.

--- DataAccess/Repository/RepositoryDb/AutoServiceDb.cs
+++ DataAccess/Repository/RepositoryDb/AutoServiceDb.cs
@@ -1,4 +1,3 @@
-using System.Data.Common;
 using System.Data.Entity;
 using DataAccess.Model;
 using MySql.Data.Entity;
@@ -12,11 +11,6 @@ namespace DataAccess.Repository.RepositoryDb
             : base(connectionString)
         {
             Configuration.LazyLoadingEnabled = false;
-        }
-
-        public AutoServiceDb(DbConnection existingConnection, bool contextsOwnsConnection)
-            : base(existingConnection, contextsOwnsConnection)
-        {
         }
 
         public DbSet<Customer> Customers { get; set; }
