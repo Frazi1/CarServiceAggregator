a53225f2c042b9a0965545f8870bf823f22b6da0
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-07-21 20:51:51 +0300

    PriceFormatConverter.

---
+++ MVVM/Converters/PriceFormatConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Mvvm.Converters
+{
+    public class PriceFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = value?.ToString();
+            if (string.IsNullOrEmpty(s)) return DependencyProperty.UnsetValue;
+            return s.ToString(culture.NumberFormat);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
