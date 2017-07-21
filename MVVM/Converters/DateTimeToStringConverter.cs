1e94d7fa99ad344808a84947a81269896d55f031
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-07-21 13:11:00 +0300

    Added DateTimeToString converter.

---
+++ MVVM/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Mvvm.Converters
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((DateTime?)value)?.ToString("g", culture.DateTimeFormat) ?? DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
